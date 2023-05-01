e28dd27b908e9dbf48de4dd1c1e8360decfa661b
VShcherbin <vshcherbin1996@gmail.com>
2023-05-01 11:29:45 +0300

    feat: create virtual keyboard, add animation , add changing lang, add action for shift and caps

---
+++ components/keys.js
@@ -0,0 +1,24 @@
+export const eventCode = [
+  ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+  ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+  ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+  ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+  ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+]
+
+export const keysEng = [
+  ["`~", "1!", "2@", "3#", "4$", "5%", "6^", "7&", "8*", "9(", "0)", "-_", "=+", "Backspace"],
+  ["Tab", "qQ", "wW", "eE", "rR", "tT", "yY", "uU", "iI", "oO", "pP", "[{", "]}", "\\|", "Delete"],
+  ["CapsLock", "aA", "sS", "dD", "fF", "gG", "hH", "jJ", "kK", "lL", ";:", "\'\"", "Enter"],
+  ["Shift", "zZ", "xX", "cC", "vV", "bB", "nN", "mM", ",<", ".>", "/?", "↑↑", "Shift"],
+  ["Ctrl", "Win", "Alt", "Space", "Alt", "←←", "↓↓", "→→", "Ctrl"],
+]
+
+
+export const keysRus = [
+  ["ёЁ", "1!", "2\"", "3№", "4;", "5%", "6:", "7?", "8*", "9(", "0)", "-_", "=+", "Backspace"],
+  ["Tab", "йЙ", "цЦ", "уУ", "кК", "еЕ", "нН", "гГ", "шШ", "щЩ", "зЗ", "хХ", "ъЪ", "\\\\", "Delete"],
+  ["CapsLock", "фФ", "ыЫ", "вВ", "аА", "пП", "рР", "оО", "лЛ", "дД", "жЖ", "эЭ", "Enter"],
+  ["Shift", "яЯ", "чЧ", "сС", "мМ", "иИ", "тТ", "ьЬ", "бБ", "юЮ", ".,", "↑↑", "Shift"],
+  ["Ctrl", "Win", "Alt", "Space", "Alt", "←←", "↓↓", "→→", "Ctrl"],
+]
---
+++ index.js
@@ -0,0 +1,156 @@
+import { eventCode, keysEng, keysRus } from "./components/keys.js"
+
+const body = document.querySelector("body")
+let ctrlIsDown = false;
+let capsIsEnable = false;
+let keys = document.getElementsByClassName("keyboard__key")
+console.log(keys)
+
+function newElement(tag, cssClass) {
+  let element = document.createElement(tag);
+  element.classList.add(cssClass)
+  return element
+}
+
+
+function init() {
+
+  let container = newElement("div", "container")
+  body.prepend(container)
+
+  let title = newElement("div", "title")
+  title.innerText = "RSS Виртуальная клавиатура"
+  container.append(title)
+
+  let textarea = newElement("textarea", "textarea")
+  container.append(textarea)
+
+  let keyboard = newElement("div", "keyboard")
+  container.append(keyboard)
+  for (let i = 0; i < eventCode.length; i++) {
+    let keyboardRow = newElement("div", "keyboard__row")
+    keyboard.append(keyboardRow)
+    for (let j = 0; j < eventCode[i].length; j++) {
+      let keyboardKey = newElement("div", "keyboard__key")
+      keyboardKey.setAttribute("id", eventCode[i][j]);
+      (keysEng[i][j].length < 3) ? keyboardKey.innerText = keysEng[i][j][0] : keyboardKey.innerText = keysEng[i][j]; // If length more than 2 it means there is special key
+      keyboardRow.append(keyboardKey)
+
+    }
+
+  }
+
+  let aboutOS = newElement("div", "aboutOS")
+  aboutOS.innerText = "Клавиатура создана в операционной системе Windows"
+  container.append(aboutOS)
+
+  let switchLang = newElement("div", "switch-lang")
+  switchLang.innerText = "Для переключения языка комбинация: левыe ctrl + alt"
+  container.append(switchLang)
+
+
+}
+
+init()
+
+// change active key during keydown
+document.addEventListener("keydown", (event) => {
+  let activeKey = document.getElementById(event.code);
+  activeKey.classList.add("active")
+});
+
+document.addEventListener("keyup", (event) => {
+  let activeKey = document.getElementById(event.code);
+  activeKey.classList.remove("active")
+});
+
+document.querySelectorAll(".keyboard__key").forEach(elem => {
+  elem.addEventListener("mousedown", function (event) {
+    let code = this.getAttribute("id")
+    this.classList.add("active")
+  })
+  elem.addEventListener("mouseup", function (event) {
+    let code = this.getAttribute("id")
+    this.classList.remove("active")
+  })
+});
+
+
+// change lang
+document.addEventListener("keydown", function (event) {
+  if (event.key == "Control") ctrlIsDown = true;
+  if (event.key == "Alt" && ctrlIsDown) {
+    ctrlIsDown = false;
+    if (document.getElementById("KeyQ").innerHTML === "q" || document.getElementById("KeyQ").innerHTML === "Q") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayRusKeys = [].concat(...keysRus);
+        (arrayRusKeys[i].length < 3) ? keys[i].innerHTML = arrayRusKeys[i][0] : keys[i].innerHTML = arrayRusKeys[i];
+      }
+    } else if (document.getElementById("KeyQ").innerHTML === "й" || document.getElementById("KeyQ").innerHTML === "Й") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayEngKeys = [].concat(...keysEng);
+        (arrayEngKeys[i].length < 3) ? keys[i].innerHTML = arrayEngKeys[i][0] : keys[i].innerHTML = arrayEngKeys[i];
+      }
+    }
+  }
+});
+
+// change keys while shift is down
+document.addEventListener("keydown", function (event) {
+  if (event.key == "Shift") {
+    if (document.getElementById("KeyQ").innerHTML === "й" || document.getElementById("KeyQ").innerHTML === "Й") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayRusKeys = [].concat(...keysRus);
+        (arrayRusKeys[i].length < 3) ? keys[i].innerHTML = arrayRusKeys[i][1] : keys[i].innerHTML = arrayRusKeys[i];
+      }
+    } else if (document.getElementById("KeyQ").innerHTML === "q" || document.getElementById("KeyQ").innerHTML === "Q") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayEngKeys = [].concat(...keysEng);
+        (arrayEngKeys[i].length < 3) ? keys[i].innerHTML = arrayEngKeys[i][1] : keys[i].innerHTML = arrayEngKeys[i];
+      }
+    }
+  }
+})
+
+document.addEventListener("keyup", function (event) {
+  if (event.key == "Shift") {
+    if (document.getElementById("KeyQ").innerHTML === "й" || document.getElementById("KeyQ").innerHTML === "Й") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayRusKeys = [].concat(...keysRus);
+        (arrayRusKeys[i].length < 3) ? keys[i].innerHTML = arrayRusKeys[i][0] : keys[i].innerHTML = arrayRusKeys[i];
+      }
+    } else if (document.getElementById("KeyQ").innerHTML === "q" || document.getElementById("KeyQ").innerHTML === "Q") {
+      for (let i = 0; i < keys.length; i++) {
+        let arrayEngKeys = [].concat(...keysEng);
+        (arrayEngKeys[i].length < 3) ? keys[i].innerHTML = arrayEngKeys[i][0] : keys[i].innerHTML = arrayEngKeys[i];
+      }
+    }
+  }
+})
+
+
+// change keys while caps is down
+document.addEventListener("keydown", function (event) {
+  if (event.key == "CapsLock") {
+    if (capsIsEnable === false) {
+      capsIsEnable = true;
+      for (let i = 0; i < keys.length; i++) {
+        if (keys[i].innerHTML.length === 1 && keys[i].innerHTML.match(/[a-zа-яё]/))
+          keys[i].innerHTML = keys[i].innerHTML.toUpperCase()
+      }
+
+    } else if (capsIsEnable === true) {
+      capsIsEnable = false;
+      for (let i = 0; i < keys.length; i++) {
+        if (keys[i].innerHTML.length === 1 && keys[i].innerHTML.match(/[A-ZА-ЯЁ]/))
+          keys[i].innerHTML = keys[i].innerHTML.toLowerCase()
+      }
+    }
+  }
+
+})
+
+
+
+
+
